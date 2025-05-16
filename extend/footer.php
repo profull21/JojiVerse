c5742b4619d1dfb7902e51b699a0c55799424e66
profull21 <omarpro2112@gmail.com>
2025-05-15 19:50:49 -0600

    Add files via upload

---
+++ extend/footer.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+<p></p>
+<footer class="footer">
+    <p></p>
+    <nav>
+        <ul>
+            <li>
+                <a href="https://youtube.com/shorts/d21qBrtyjDg?si=LTR5-B0uLNcXczDI" target="_blank">
+                    <i class="fab fa-youtube"></i> Datos Curiosos
+                </a>
+            </li>
+            <li>
+                <a href="https://youtube.com/shorts/QYXxjnmv6Uk?si=WUHMyVco1IaMc_9U" target="_blank">
+                    <i class="fab fa-youtube"></i> JOJI en un minuto
+                </a>
+            </li>
+            <li>
+                <a href="https://youtube.com/shorts/oPv2k5MZHP4?si=3zxvqV19NYtYbWXb" target="_blank">
+                    <i class="fab fa-youtube"></i> TOP canciones 
+                </a>
+            </li>
+        </ul>
+    </nav>
+   			</footer>
+		</div>
+	</body>
+</html>
---
+++ extend/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Joji Verse</title>
+	<meta content="">
+	<link rel="icon" type="img/x-icon" href="img/albumj4.ico">
+	<meta name="title" content="JojiVerse  ">
+	<meta name="description" content="aca de jose jose ">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="container">
+		<header>
+			<nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark" >
+  				<div class="container-fluid">
+    				<a class="navbar-brand" href="index.php">JOJI ARCHIVE</a>
+    				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+     				<span class="navbar-toggler-icon"></span>
+    				</button>
+   			    	<div class="collapse navbar-collapse" id="navbarNav">
+      				<ul class="navbar-nav">
+       			 	<li class="nav-item">
+          			<a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+        			</li>
+        			<li class="nav-item">
+          			<a class="nav-link" href="historia.php">Historia</a>
+        			</li>
+        			<li class="nav-item">
+          			<a class="nav-link" href="discografia.php">Discografía</a>
+        			</li>
+        			<li class="nav-item">
+          			<a class="nav-link" href="registro.php">registrate</a>
+        			</li>
+      				</ul>
+    				</div>
+  				</div>
+			</nav>
+		</header>
+		
+
+		
